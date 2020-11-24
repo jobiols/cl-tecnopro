35dad729bb270498bda8eb3ca687e6757006ae18
Jorge Obiols <jorge.obiols@gmail.com>
2020-11-24 16:09:29 -0300

    [FIX] poner controllers como json POST

--- payment_vpos/controllers/main.py
+++ payment_vpos/controllers/main.py
@@ -24,12 +24,12 @@ class VPosController(http.Controller):
         _logger.error('Error interno al procesar datos')
         return http.request.render('payment_vpos.vpos_error', {})
 
-    @http.route('/bancard/return_url', type='http', auth='public', csrf=False)
+    @http.route('/bancard/return_url', type='json', auth='public', methods=['POST'], csrf=False)
     def vpos_bancard_return_url(self, **kw):
         _logger.info('Respuesta de Bancard (ok o error)')
         return http.request.render('payment_vpos.vpos_approved', {})
 
-    @http.route('/bancard/cancelled', type='http', auth='public', csrf=False)
+    @http.route('/bancard/cancelled', type='json', auth='public', methods=['POST'], csrf=False)
     def vpos_bancard_cancelled(self, **kw):
         _logger.info('Respuesta de Bancard Cancelacion')
         return http.request.render('payment_vpos.vpos_cancelled', {})
