a5aa63164079c1960d1e6e08ab47ce45c945833a
Gabriela Rivero <regaby@gmail.com>
2020-12-12 23:10:31 -0300

    js

--- payment_vpos/static/src/js/bancard.js
+++ payment_vpos/static/src/js/bancard.js
@@ -7,15 +7,4 @@
     var _t = core._t;
     var ajax = require(web.ajax);
 
-    // sAnimation.registry.js_send_json_request = sAnimation.Class.extend({
-    // function (require) {
-    //     ajax.jsonRpc("/bancard/return_url", 'call', {}).done(function() {
-    //     }).then(function (json_data) {
-    //         self.view.$el.append(QWeb.render('payment_vpos.show_answer',
-    //         {'data': json_data}));
-    //         // $("#ViewCartModal").modal(); // Display modal
-    //         return ;
-    //         })
-    //     }//
-    // })
 })
